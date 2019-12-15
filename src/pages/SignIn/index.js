b6c8f9dde1da89e92cdec7ecb317509f97587e64
Hikaro Cordeiro <hikaro.cordeiro@gmail.com>
2019-12-14 23:25:53 -0300

    signIn page style

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Image } from 'react-native';
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.png';
+
+import { Container, Form, Input, SubmitButton } from './styles';
 
 export default function SignIn() {
-  return <View />;
+  return (
+    <Container>
+      <Image source={logo} />
+      <Form>
+        <Input
+          keyboardType="numeric"
+          autoCorrect={false}
+          placeholder="Informe seu ID de cadastro"
+        />
+        <SubmitButton onPress={() => {}}>Entrar no sistema</SubmitButton>
+      </Form>
+    </Container>
+  );
 }
