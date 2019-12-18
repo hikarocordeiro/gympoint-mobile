1a0e01873f8bcef10eeea0351b0fde30d77c4d85
Hikaro Cordeiro <hikaro.cordeiro@gmail.com>
2019-12-18 11:48:19 -0300

    add question

--- src/pages/Help/Add/index.js
+++ src/pages/Help/Add/index.js
@@ -1,16 +1,55 @@
-import React from 'react';
-import { TouchableOpacity } from 'react-native';
+import React, { useState, useCallback } from 'react';
+import { useSelector } from 'react-redux';
+import { TouchableOpacity, Alert } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import api from '~/services/api';
 
 import Header from '~/components/Header';
 
-import { Container } from './styles';
+import { Container, QuestionInput, SubmitButton } from './styles';
 
-export default function Add() {
+export default function Add({ navigation }) {
+  const [question, setQuestion] = useState();
+  const [loading, setLoading] = useState(false);
+
+  const { id } = useSelector(state => state.auth.student);
+
+  const handleSubmit = useCallback(async () => {
+    try {
+      setLoading(true);
+
+      await api.post(`/students/${id}/help-orders`, { question });
+
+      setLoading(false);
+
+      navigation.navigate('List');
+    } catch (err) {
+      Alert.alert(
+        'Falha ao pedir ajuda',
+        (err.response && err.response.data.error) || 'Tente novamente'
+      );
+      setLoading(false);
+    }
+  }, [id, navigation, question]);
+
   return (
     <>
       <Container>
         <Header />
+
+        <QuestionInput
+          autoCorrect={false}
+          placeholder="Inclua seu pedido de auxílio"
+          multiline
+          numberOfLines={15}
+          value={question}
+          onChangeText={setQuestion}
+        />
+
+        <SubmitButton loading={loading} onPress={handleSubmit}>
+          Enviar pedido
+        </SubmitButton>
       </Container>
     </>
   );
